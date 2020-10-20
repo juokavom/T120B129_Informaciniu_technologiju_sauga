af502eeb60d598ef153ca19fab0c493934123113
Jokubas Akramas <j.akramas@gmail.com>
2020-10-20 10:20:14 +0300

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Lima yankee");
+        System.out.println("Java version: " + Runtime.version());
+    }
+}
